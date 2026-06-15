b5af6642918450b9d5e4f056a4716f902a6a435f
Tuan Pham <tuanqpham0921@gmail.com>
2026-06-15 10:21:49 -0500

    initial domain expansion to test orchestration

---
+++ backend/app/domains/project/schemas/request_schemas.py
@@ -0,0 +1,73 @@
+"""
+Classification schemas for project domain strategies
+Use for query the project's information or update the project's information database
+Need semantic parsing in the chat to understand the project's request
+manual or traditional project info update is going to be a standard endpoint instead
+
+support query:
+- Can you send a feedback to the project? (HITL)
+- Can you tell me about the project?
+
+Future Plan:
+- support how many active user is using the system
+- support how many active developers is using the system
+- as an admin, add a new developer to the system giving access to admin capabilities
+- as an admin, monitor how much total token is used by the system
+- as an admin, what's the average response time of the system
+
+This is where you can really incorperate the agentic capabilities to the system
+so you can log in as an admin, or develop more advanced features to the system
+"""
+
+from typing import Optional, Literal, List
+from uuid import uuid4
+from pydantic import Field
+from app.common.base_node import BaseNode
+from enum import Enum
+from typing import Any
+from app.domains.project.types import NodeType
+
+class FeedbackRequest(BaseNode):
+    """Classification schema for Feedback request"""
+    node_type: Literal[NodeType.FEEDBACK] = NodeType.FEEDBACK
+    # NOTE: good place to have a simple HITL (Human In The Loop) for feedback
+    # something like awesome "can you please provide your email so we can get back to you? if not it's ok too"
+    contact_info: str = Field(default=None, description=
+                              "Contact information of the user providing the feedback (email, phone, etc.)"
+                            )
+    feedback: str = Field(..., description="User's feedback to the project")
+    
+    def model_post_init(self, __context) -> None:
+        if not self.refusal:
+            base_id = str(uuid4())[:8]
+            self.id = base_id + "_feedback_req"
+        super().model_post_init(__context)
+    
+    def get_type(self) -> NodeType:
+        return NodeType.FEEDBACK
+    
+class ProjectInfoField(str, Enum):
+    NAME = "name"
+    DESCRIPTION = "description"
+    TECHNOLOGY_STACK = "technology_stack"
+    PROJECT_URL = "project_url"
+    PROJECT_GITHUB_URL = "project_github_url"
+    PROJECT_GITHUB_REPO_NAME = "project_github_repo_name"
+    PROJECT_GITHUB_REPO_URL = "project_github_repo_url"
+    PROJECT_GITHUB_REPO_URL = "project_github_repo_url"
+    ALL = "all"
+    
+class ProjectInfoRequest(BaseNode):
+    """Classification schema for Project Info request"""
+    node_type: Literal[NodeType.PROJECT_INFO] = NodeType.PROJECT_INFO
+    fields: list[ProjectInfoField] = Field(..., description="Fields to update or retrieve")
+    values: dict[str, Any] = Field(default_factory=dict, description="Values to update or retrieve")
+    
+    def model_post_init(self, __context) -> None:
+        if not self.refusal:
+            base_id = str(uuid4())[:8]
+            self.id = base_id + "_project_info_req"
+        super().model_post_init(__context)
+    
+    def get_type(self) -> NodeType:
+        return NodeType.PROJECT_INFO
---
+++ backend/app/domains/users/schemas/request_schemas.py
@@ -0,0 +1,92 @@
+"""
+Classification schemas for user domain strategies
+Use for query the user's information or update the user's information database
+Need semantic parsing in the chat to understand the user's request
+manual or traditional user info update is going to be a standard endpoint instead
+
+General Idea:
+- Assistant: "What is your preferences for books? are you a developer?"
+- User: "I'm a developer / recruiter. I care more about the internal working of the system"
+- Tool: "Update user info to reflect the user's preferences"
+- Assistant: 
+"Got it! I've remembered your background. 
+Now I can:
+1. recommend books that are more relevant to you.
+2. give you the developer information
+3. give technology stack information or how the system is built
+
+Let me know what way you want to go.
+"
+
+Future Plan (agentic capabilities):
+- support send the developer I'm intersted in contributing to the project
+- support how many active user is using the system
+- support how many active developers is using the system
+- as an admin, add a new developer to the system giving them access to update their own information
+"""
+
+from typing import Optional, Literal, List
+from uuid import uuid4
+from pydantic import Field
+from app.common.base_node import BaseNode
+from enum import Enum
+from typing import Any
+
+from app.domains.users.types import NodeType
+
+class UserInfoAction(str, Enum):
+    GET = "get"
+    UPDATE = "update"
+
+class UserInfoField(str, Enum):
+    NAME = "name"
+    AGE = "age"
+    BIO = "bio"
+    TOKEN_USAGE = "token_usage"
+    SAVED_MEMORY = "saved_memory"
+    PREVIOUS_CONVERSATION = "previous_conversation"
+    CURRENT_CONVERSATION = "current_conversation"
+    ALL = "all"
+
+class UserInfoRequest(BaseNode):
+    action: UserInfoAction = Field(..., description="Action to perform on user info")
+    fields: list[UserInfoField] = Field(..., description="Fields to update or retrieve")
+    values: dict[str, Any] = Field(default_factory=dict, description="Values to update or retrieve")
+    
+    def model_post_init(self, __context) -> None:
+        if not self.refusal:
+            base_id = str(uuid4())[:8]
+            self.id = base_id + "_usr_req"
+        super().model_post_init(__context)
+    
+    def get_type(self) -> NodeType:
+        return NodeType.USER_INFO_REQUEST
+
+class DeveloperInfoField(str, Enum):
+    NAME = "name"
+    BIO = "bio"
+    EMAIL = "email"
+    LINKEDIN_URL = "linkedin_url"
+    ALL = "all"
+    
+# NOTE: This is to get my own information from the database
+# we can use update to update my own information (required developer access)
+# workflow can check if the request user is a developer to update their own information
+# going towards agentic capabilities. If the validation fails, then we can refuse the request.
+# or just safegaurd to GET functionality only
+class DeveloperInfoRequest(BaseNode):
+    """Classification schema for Developer Info request"""
+    node_type: Literal[NodeType.DEVELOPER_INFO] = NodeType.DEVELOPER_INFO
+    action: UserInfoAction = Field(..., description="Action to perform on user info")
+
+    fields: list[DeveloperInfoField] = Field(..., description="Fields to update or retrieve")
+    values: dict[str, Any] = Field(default_factory=dict, description="Values to update or retrieve")
+    
+    def model_post_init(self, __context) -> None:
+        if not self.refusal:
+            base_id = str(uuid4())[:8]
+            self.id = base_id + "_dev_req"
+        super().model_post_init(__context)
+    
+    def get_type(self) -> NodeType:
+        return NodeType.DEVELOPER_INFO
