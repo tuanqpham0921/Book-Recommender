60f34b37d5992b0ff7a2769ed89987d8b63be12c
Tuan Pham <tuanqpham0921@gmail.com>
2026-06-26 12:13:59 -0500

    added a random depndencies generator

--- backend/tests/fake_task_generator.py
+++ backend/tests/fake_task_generator.py
@@ -1,18 +1,21 @@
 from app.domains.base_request import AnalyzeBaseRequest
 from app.domains.node_types import UnknownNodeTypeEnum
 from pydantic import Field
-from typing import Literal
 from app.domains.base_request import BaseRequest
+import random
 
 class FakeTask(AnalyzeBaseRequest):
     node_type: UnknownNodeTypeEnum = UnknownNodeTypeEnum.UNKNOWN
     description: str = Field(default="Fake task", description="Description of the fake task")
 
+def get_random_dependes_on(execution_order: list[str]) -> list[str]:
+    num_list = random.randint(1, len(execution_order))
+    return random.sample(execution_order, num_list)
 
 def generate_fake_tasks(accepted: list[BaseRequest], execution_order: list[str]) -> list[FakeTask]:
     for i in range(10):
         fake_task = FakeTask(
-            depends_on=[execution_order[-1]],
+            depends_on=get_random_dependes_on(execution_order),
             target_goal = ["goal_1"],
             id = f"task_{i}",
             description = f"Fake task {i}",
